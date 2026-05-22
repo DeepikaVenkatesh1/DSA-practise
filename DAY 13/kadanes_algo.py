0df9291b49d1480e0935158c2ab0662d4a9a592e
Deepika V <deepikavenkatesh109@gmail.com>
2026-05-22 21:08:15 +0530

    kadanes

--- DAY 13/kadanes_algo.py
+++ DAY 13/kadanes_algo.py
@@ -1,12 +1,11 @@
 class Solution:
     def kadanes(self,nums):
         cur = 0
-        maxi = nums[0]
+        maxi=0
         for n in nums:
             cur+=n
-            maxi=max(cur,maxi)
-            if cur<0:
+            maxi=max(maxi,cur)
+            if n<0:
                 cur=0
         return maxi
-sol=Solution()
-print(sol.kadanes([-2,1,-3,4,-1,2,1,-5,4]))
+print(Solution().kadanes([-2,1,-3,4,-1,2,1,-5,4]))
