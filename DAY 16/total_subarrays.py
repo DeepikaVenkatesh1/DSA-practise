4de0156048ba6556343f9977625d1ea972f9a9ba
Deepika V <deepikavenkatesh109@gmail.com>
2026-06-12 19:48:19 +0530

    total sunarrays

--- DAY 16/total_subarrays.py
+++ DAY 16/total_subarrays.py
@@ -1,20 +1,16 @@
 class Solution:
-    def max_subarray(self,nums,k):
-        prefix=0
-        seen={0:1}
-        count=0
-        for i in range(len(nums)):
-            prefix+=nums[i]
-            seen[prefix]=i
+    def total_subarrays(self, nums, k):
+        prefix = 0
+        count = 0
+        seen = {0: 1}
 
-            if (prefix-k) in seen:
-                count+=seen[prefix-k]
-            
-            if prefix in seen:
-                seen[prefix]+=1
-            else:
-                seen[prefix]=1
+        for i in range(len(nums)):
+            prefix += nums[i]
+
+            count += seen.get(prefix - k, 0)
+
+            seen[prefix] = seen.get(prefix, 0) + 1
+
         return count
-sol=Solution()
-print(sol.max_subarray([1,-1,5,-2,3],3))
-print(sol.max_subarray([1,1,2,1],3))
+sol = Solution()
+print(sol.total_subarrays([1, 1, 1], 2))
