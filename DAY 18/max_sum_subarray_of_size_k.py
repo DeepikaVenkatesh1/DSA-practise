142f918e1a93b47331225ca3575e679b41fd2729
Deepika V <deepikavenkatesh109@gmail.com>
2026-06-15 21:16:07 +0530

    max sum subarray of k

--- DAY 18/max_sum_subarray_of_size_k.py
+++ DAY 18/max_sum_subarray_of_size_k.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxSumSubarray(self,nums,k):
+        left=0
         window=sum(nums[:k])
         maxi=window
-        for i in range(k,len(nums)):
-            window+=nums[i]-nums[i-k]
+        for right in range(k,len(nums)):
+            window+=nums[right]-nums[right-k]
             maxi=max(maxi,window)
         return maxi
 sol=Solution()
 print(sol.maxSumSubarray([1,12,-5,-6,50,3],4))
-
-
 
